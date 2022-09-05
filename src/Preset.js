4d0e1d87019b864384049fc9155049efe5801d92
Shogoofa Akbari <shogoofa.musket@gmail.com>
2022-09-05 02:14:31 -0700

    added state/functionality for buttons

--- src/Preset.js
+++ src/Preset.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import 'bootstrap/dist/css/bootstrap.css';
-import Header from "./components/Header"
-import Nav from "./components/Nav"
-import About from "./components/About"
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio"
+import Resume from "./components/Resume"
+import Contact from "./components/Contact"
+
 
 import "./index.css";
 
 export default function Preset() {
+    const [page, setPage] = useState(<About/>)
     return(
         <div>
             <Header/>
-            <Nav/>
-            <About/>
+            <div className="Content-Container">
+            <Nav setPage={setPage} About={About} Portfolio={Portfolio} Resume={Resume} Contact={Contact}/>
+            {page}
+            </div>
+            {/* <About/> */}
         </div>
     )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "../index.css"
 
-function Nav() {
+function Nav({setPage, About, Portfolio, Resume, Contact}) {
     return (
     <>
         <div className="navBar">
-            <button className="btn"> About Me</button>
-            <button className="btn">Portfolio</button>
-            <button className="btn">Resume</button>
-            <button className="btn">Contact</button>
+            <button onClick={() => setPage(<About/>)} className="btn"> About Me</button>
+            <button onClick={() => setPage(<Portfolio/>)} className="btn">Portfolio</button>
+            <button onClick={() => setPage(<Resume/>)} className="btn">Resume</button>
+            <button onClick={() => setPage(<Contact/>)} className="btn">Contact</button>
         </div>
     </>
     )
