89e03bfb90ab1cb1aaec39b1d62b148369d88c76
Shogoofa Akbari <shogoofa.musket@gmail.com>
2022-09-06 19:31:14 -0700

    added images as placeholder, plus link &  styled imaged to space around

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,9 +1,44 @@
 import React from "react";
+import CarpeDiem from "../Images/CarpeDiem.png"
 import "../index.css"
 
 function Portfolio() {
     return (
-       <div>Portfolio</div>
+        <>
+        <div className="content-Container" >
+            <div className="box1">
+                <img src={CarpeDiem} width="300rem" height= "200rem" alt="Logo" />
+                <div class="caption">Carpe Diem</div>
+                <a className="Link"href="https://github.com/ShogoofaA/Carpe-Diem" target="_blank" rel="noopener noreferrer">Link to Project</a>
+            </div>
+            <div className="box2">
+                <img src={CarpeDiem} width="300rem" height= "200rem" alt="Logo" />
+                <div class="caption">Carpe Diem</div>
+                <a className="Link"href="https://github.com/ShogoofaA/Carpe-Diem" target="_blank" rel="noopener noreferrer">Link to Project</a>
+            </div>
+            <div className="box3">
+                <img src={CarpeDiem} width="300rem" height= "200rem" alt="Logo" />
+                <div class="caption">Carpe Diem</div>
+                <a className="Link"href="https://github.com/ShogoofaA/Carpe-Diem" target="_blank" rel="noopener noreferrer">Link to Project</a>
+            </div>
+            <div className="box4">
+                <img src={CarpeDiem} width="300rem" height= "200rem" alt="Logo" />
+                <div class="caption">Carpe Diem</div>
+                <a className="Link"href="https://github.com/ShogoofaA/Carpe-Diem" target="_blank" rel="noopener noreferrer">Link to Project</a>
+            </div>
+            <div className="box4">
+                <img src={CarpeDiem} width="300rem" height= "200rem" alt="Logo" />
+                <div class="caption">Carpe Diem</div>
+                <a className="Link"href="https://github.com/ShogoofaA/Carpe-Diem" target="_blank" rel="noopener noreferrer">Link to Project</a>
+            </div>
+            <div className="box4">
+                <img src={CarpeDiem} width="300rem" height= "200rem" alt="Logo" />
+                <div class="caption">Carpe Diem</div>
+                <a className="Link"href="https://github.com/ShogoofaA/Carpe-Diem" target="_blank" rel="noopener noreferrer">Link to Project</a>
+            </div>
+        </div>
+      
+        </>
     )
 }
 
