fd68fe2854b1e67fed00af12294102ab136f2a19
Shogoofa Akbari <shogoofa.musket@gmail.com>
2022-09-06 18:57:53 -0700

    added resume plus styling

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,7 +3,9 @@ import "../index.css"
 
 function Resume() {
     return (
-        <div>Resume</div>
+        <div className="Resume">
+            <a href="https://docs.google.com/document/d/1i8a_369JIk1OqQG81AHRY2yJJPpq72WnRD8HQz28PEg/edit?usp=sharing" className="Res"download class="color">Please click to view my Resume!</a>
+        </div>
     )
 }
 
